7b079fdbd29b60deed6e85f2f2209a1d189f9f9c
[VivekGarg1] <[vivekgarg020@gmail.com]>
2020-07-31 09:21:48 +0530

    code merge for Spring scope

--- SpringCore/src/com/home/spring/client/ApplicationContextContainer.java
+++ SpringCore/src/com/home/spring/client/ApplicationContextContainer.java
@@ -1,31 +1,38 @@
 package com.home.spring.client;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.home.spring.model.Message;
 
 public class ApplicationContextContainer {
 
 	public static void main(String[] args) {
-		
-		ApplicationContext context=new ClassPathXmlApplicationContext("ApplicationContext.xml");
-		Message message = context.getBean("message", Message.class);
-		System.out.println(message.getMessageId() + "\t" + message.getMessage());
-		
-		System.out.println("----------------------------------");
-		Object object = context.getBean("message");
-		if (object != null) {
-			Message message1 = (Message) object;
-			System.out.println(message1.getMessageId() + "\t" + message1.getMessage());
-		}
-		
-		System.out.println("----------------------------------");
-		
-		
-		((AbstractApplicationContext) context).close();
 
+		ApplicationContext context = null;
+		try {
+			// context=new ClassPathXmlApplicationContext("ApplicationContext.xml");
+			// context = new FileSystemXmlApplicationContext("G:\\Java Development\\git\\SpringCore\\SpringCore\\src\\ApplicationContext.xml");
+			//context = new FileSystemXmlApplicationContext("G:/Java Development/git/SpringCore/SpringCore/src/ApplicationContext.xml");
+			context = new FileSystemXmlApplicationContext("src/ApplicationContext.xml");
+			Message message = context.getBean("message", Message.class);
+			System.out.println(message.getMessageId() + "\t" + message.getMessage());
+
+			System.out.println("----------------------------------");
+			Object object = context.getBean("message");
+			if (object != null) {
+				Message message1 = (Message) object;
+				System.out.println(message1.getMessageId() + "\t" + message1.getMessage());
+			}
+		} catch (BeansException e) {
+			e.printStackTrace();
+		} finally {
+			if (context != null)
+				((AbstractApplicationContext) context).close();
+		}
 	}
 
 }
---
+++ SpringCore/src/com/home/spring/client/SpringScopes.java
@@ -0,0 +1,23 @@
+package com.home.spring.client;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.home.spring.model.Message;
+
+public class SpringScopes {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context=new ClassPathXmlApplicationContext("BeansScopes.xml");
+		 Message message = context.getBean("message", Message.class);
+		 message.setMessageId(1001);
+		 message.setMessage("Hello Vivek!!!");
+		 System.out.println(message.getMessageId()+"\t"+message.getMessage());
+		 
+		 Message message1 = context.getBean("message", Message.class);
+		 System.out.println(message1.getMessageId()+"\t"+message1.getMessage());
+
+	}
+
+}
